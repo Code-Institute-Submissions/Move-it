d328274a22fd058173d34227d97fb0a714666ae3
shanehowley <snhowley@gmail.com>
2017-03-28 11:46:49 +0100

    deploying

--- Move_it/settings.py
+++ Move_it/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v4fyx#s4@5!3%5e$8zh59kftt_s)cudf8iuyqt^3(0t&p-(f0q'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,12 +79,16 @@ WSGI_APPLICATION = 'Move_it.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+
+
+# for running locally
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
---
+++ env.sh
@@ -0,0 +1,10 @@
+echo 'Setting SECRET_KEY'
+export SECRET_KEY='sv4fyx#s4@5!3%5e$8zh59kftt_s)cudf8iuyqt^3(0t&p-(f0q'
+
+echo 'Setting DATABASE_URL'
+export DATABASE_URL='postgres://ukppdzjojadxgo:265bdea67b9d0662c520e50e0a1ee0e30c3deef88fcedf35b44eaef01d95825a@ec2-54-217-235-11.eu-west-1.compute.amazonaws.com:5432/d2o4sk8p1vpg0l'
+echo 'Done'
+
+
+postgres://ukppdzjojadxgo:265bdea67b9d0662c520e50e0a1ee0e30c3deef88fcedf35b44eaef01d95825a@ec2-54-217-235-11.eu-west-1.compute.amazonaws.com:5432/d2o4sk8p1vpg0l
+postgres://user:password@host/database
